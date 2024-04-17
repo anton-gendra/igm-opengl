2928c4e076a6c9106a46972ae47991e14349cfc6
Juan <jjusto2000@gmail.com>
2024-04-17 23:35:36 +0200

    Modificada textura para que se vea correcta

--- practica_cubo.cpp
+++ practica_cubo.cpp
@@ -11,31 +11,30 @@
 
 // GLM library to deal with matrix operations
 #include <glm/glm.hpp>
-#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/mat4x4.hpp>               // glm::mat4
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::perspective
 #include <glm/gtc/type_ptr.hpp>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-
 int gl_width = 640;
 int gl_height = 480;
 
-
-void glfw_window_size_callback(GLFWwindow* window, int width, int height);
+void glfw_window_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 void render(double);
 
-GLuint shader_program = 0; // shader program to set render pipeline
-GLuint vao = 0; // Vertext Array Object to set input data
-GLuint texture = 0;  // Texture to paste on polygon
+GLuint shader_program = 0;        // shader program to set render pipeline
+GLuint vao = 0;                   // Vertext Array Object to set input data
+GLuint texture = 0;               // Texture to paste on polygon
 GLint mv_location, proj_location; // Uniforms for transformation matrices
 
-int main() {
-  
- 
+int main()
+{
+
   // start GL context and O/S window using the GLFW helper library
-  if (!glfwInit()) {
+  if (!glfwInit())
+  {
     fprintf(stderr, "ERROR: could not start GLFW3\n");
     return 1;
   }
@@ -45,8 +44,9 @@ int main() {
   //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(gl_width, gl_height, "My spinning cube", NULL, NULL);
-  if (!window) {
+  GLFWwindow *window = glfwCreateWindow(gl_width, gl_height, "My spinning cube", NULL, NULL);
+  if (!window)
+  {
     fprintf(stderr, "ERROR: could not open window with GLFW3\n");
     glfwTerminate();
     return 1;
@@ -59,10 +59,10 @@ int main() {
   glewInit();
 
   // get version info
-  const GLubyte* vendor = glGetString(GL_VENDOR); // get vendor string
-  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-  const GLubyte* glversion = glGetString(GL_VERSION); // version as a string
-  const GLubyte* glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION); // version as a string
+  const GLubyte *vendor = glGetString(GL_VENDOR);                        // get vendor string
+  const GLubyte *renderer = glGetString(GL_RENDERER);                    // get renderer string
+  const GLubyte *glversion = glGetString(GL_VERSION);                    // version as a string
+  const GLubyte *glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION); // version as a string
   printf("Vendor: %s\n", vendor);
   printf("Renderer: %s\n", renderer);
   printf("OpenGL version supported %s\n", glversion);
@@ -74,36 +74,35 @@ int main() {
   glDepthFunc(GL_LESS); // set a smaller value as "closer"
 
   // Vertex Shader
-   const char* vertex_shader =
-        "#version 130\n"
-        "in vec4 v_pos;\n"
-        "in vec2 texCoord;\n" // Added texture coordinate input
-        "out vec2 fragTexCoord;\n" // Pass texture coordinate to fragment shader
-       "out vec4 vs_color;\n"
-        "uniform mat4 mv_matrix;\n"
-        "uniform mat4 proj_matrix;\n"
-        "void main() {\n"
-        "  gl_Position = proj_matrix * mv_matrix * v_pos;\n"
-        "  fragTexCoord = texCoord;\n" // Pass texture coordinate
-       "  vs_color = v_pos * 2.0 + vec4(0.4, 0.4, 0.4, 0.0);\n"
-        "}\n";
+  const char *vertex_shader =
+      "#version 130\n"
+      "in vec4 v_pos;\n"
+      "in vec2 texCoord;\n"      // Added texture coordinate input
+      "out vec2 fragTexCoord;\n" // Pass texture coordinate to fragment shader
+      "out vec4 vs_color;\n"
+      "uniform mat4 mv_matrix;\n"
+      "uniform mat4 proj_matrix;\n"
+      "void main() {\n"
+      "  gl_Position = proj_matrix * mv_matrix * v_pos;\n"
+      "  fragTexCoord = texCoord;\n" // Pass texture coordinate
+      "  vs_color = v_pos * 2.0 + vec4(0.4, 0.4, 0.4, 0.0);\n"
+      "}\n";
 
   // Fragment Shader
-   const char* fragment_shader =
-       "#version 130\n"
-       "in vec2 fragTexCoord;\n" // Received texture coordinate
-       "in vec4 vs_color;\n"
-       "out vec4 frag_color;\n"
-       "uniform sampler2D tex;\n" // Texture sampler
-       "uniform bool applyTexture;\n" // Control the texture application
-       "void main() {\n"
-       "  if (applyTexture) {\n"
-       "    frag_color = texture(tex, fragTexCoord);\n" // Apply texture
-       "  } else {\n"
-       "    frag_color = vs_color;\n" // Default color for other faces
-       "  }\n"
-       "}\n";
-
+  const char *fragment_shader =
+      "#version 130\n"
+      "in vec2 fragTexCoord;\n" // Received texture coordinate
+      "in vec4 vs_color;\n"
+      "out vec4 frag_color;\n"
+      "uniform sampler2D tex;\n"     // Texture sampler
+      "uniform bool applyTexture;\n" // Control the texture application
+      "void main() {\n"
+      "  if (applyTexture) {\n"
+      "    frag_color = texture(tex, fragTexCoord);\n" // Apply texture
+      "  } else {\n"
+      "    frag_color = vs_color;\n" // Default color for other faces
+      "  }\n"
+      "}\n";
 
   // Shaders compilation
   GLuint vs = glCreateShader(GL_VERTEX_SHADER);
@@ -137,67 +136,65 @@ int main() {
   //       6        5
   //
   const GLfloat vertex_positions[] = {
-    -0.25f, -0.25f, -0.25f, // 1
-    -0.25f,  0.25f, -0.25f, // 0
-     0.25f, -0.25f, -0.25f, // 2
+      -0.25f, -0.25f, -0.25f, // 1
+      -0.25f, 0.25f, -0.25f,  // 0
+      0.25f, -0.25f, -0.25f,  // 2
 
-     0.25f,  0.25f, -0.25f, // 3
-     0.25f, -0.25f, -0.25f, // 2
-    -0.25f,  0.25f, -0.25f, // 0
+      0.25f, 0.25f, -0.25f,  // 3
+      0.25f, -0.25f, -0.25f, // 2
+      -0.25f, 0.25f, -0.25f, // 0
 
-     0.25f, -0.25f, -0.25f, // 2
-     0.25f,  0.25f, -0.25f, // 3
-     0.25f, -0.25f,  0.25f, // 5
+      0.25f, -0.25f, -0.25f, // 2
+      0.25f, 0.25f, -0.25f,  // 3
+      0.25f, -0.25f, 0.25f,  // 5
 
-     0.25f,  0.25f,  0.25f, // 4
-     0.25f, -0.25f,  0.25f, // 5
-     0.25f,  0.25f, -0.25f, // 3
+      0.25f, 0.25f, 0.25f,  // 4
+      0.25f, -0.25f, 0.25f, // 5
+      0.25f, 0.25f, -0.25f, // 3
 
-     0.25f, -0.25f,  0.25f, // 5
-     0.25f,  0.25f,  0.25f, // 4
-    -0.25f, -0.25f,  0.25f, // 6
+      0.25f, -0.25f, 0.25f,  // 5
+      0.25f, 0.25f, 0.25f,   // 4
+      -0.25f, -0.25f, 0.25f, // 6
 
-    -0.25f,  0.25f,  0.25f, // 7
-    -0.25f, -0.25f,  0.25f, // 6
-     0.25f,  0.25f,  0.25f, // 4
+      -0.25f, 0.25f, 0.25f,  // 7
+      -0.25f, -0.25f, 0.25f, // 6
+      0.25f, 0.25f, 0.25f,   // 4
 
-    -0.25f, -0.25f,  0.25f, // 6
-    -0.25f,  0.25f,  0.25f, // 7
-    -0.25f, -0.25f, -0.25f, // 1
+      -0.25f, -0.25f, 0.25f,  // 6
+      -0.25f, 0.25f, 0.25f,   // 7
+      -0.25f, -0.25f, -0.25f, // 1
 
-    -0.25f,  0.25f, -0.25f, // 0
-    -0.25f, -0.25f, -0.25f, // 1
-    -0.25f,  0.25f,  0.25f, // 7
+      -0.25f, 0.25f, -0.25f,  // 0
+      -0.25f, -0.25f, -0.25f, // 1
+      -0.25f, 0.25f, 0.25f,   // 7
 
-     0.25f, -0.25f, -0.25f, // 2
-     0.25f, -0.25f,  0.25f, // 5
-    -0.25f, -0.25f, -0.25f, // 1
+      0.25f, -0.25f, -0.25f,  // 2
+      0.25f, -0.25f, 0.25f,   // 5
+      -0.25f, -0.25f, -0.25f, // 1
 
-    -0.25f, -0.25f,  0.25f, // 6
-    -0.25f, -0.25f, -0.25f, // 1
-     0.25f, -0.25f,  0.25f, // 5
+      -0.25f, -0.25f, 0.25f,  // 6
+      -0.25f, -0.25f, -0.25f, // 1
+      0.25f, -0.25f, 0.25f,   // 5
 
-     0.25f,  0.25f,  0.25f, // 4
-     0.25f,  0.25f, -0.25f, // 3
-    -0.25f,  0.25f,  0.25f, // 7
+      0.25f, 0.25f, 0.25f,  // 4
+      0.25f, 0.25f, -0.25f, // 3
+      -0.25f, 0.25f, 0.25f, // 7
 
-    -0.25f,  0.25f, -0.25f, // 0
-    -0.25f,  0.25f,  0.25f, // 7
-     0.25f,  0.25f, -0.25f  // 3
+      -0.25f, 0.25f, -0.25f, // 0
+      -0.25f, 0.25f, 0.25f,  // 7
+      0.25f, 0.25f, -0.25f   // 3
   };
 
   float texCoords[] = {
-    0.0f, 0.0f,
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 1.0f,
-};
+      1.0f, 0.0f,
+      0.0f, 0.0f,
+      1.0f, 1.0f,
 
+      0.0f, 1.0f,
+      1.0f, 1.0f,
+      0.0f, 0.0f,
+  };
 
-
-  
   // Uniforms
   // - Model-View matrix
   // - Projection matrix
@@ -234,14 +231,13 @@ int main() {
   // Create texture object
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
-  
 
   // Set the texture wrapping/filtering options (on the currently bound texture object)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  
+
   // Load image for texture
   int width, height, nrChannels;
   // Before loading the image, we flip it vertically because
@@ -250,20 +246,23 @@ int main() {
   unsigned char *data = stbi_load("texture.jpg", &width, &height, &nrChannels, 0);
   // Image from http://www.flickr.com/photos/seier/4364156221
   // CC-BY-SA 2.0
-  if (data) {
+  if (data)
+  {
     // Generate texture from image
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
+  }
+  else
+  {
     printf("Failed to load texture\n");
   }
 
   // Free image once texture is generated
   stbi_image_free(data);
 
-
   // Render loop
-  while(!glfwWindowShouldClose(window)) {
+  while (!glfwWindowShouldClose(window))
+  {
 
     processInput(window);
 
@@ -279,54 +278,57 @@ int main() {
   return 0;
 }
 
-void render(double currentTime) {
+void render(double currentTime)
+{
   float f = (float)currentTime * 0.3f;
-  
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(shader_program);
 
-    // Activa la textura
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glUniform1i(glGetUniformLocation(shader_program, "tex"), 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glUseProgram(shader_program);
 
-    // Configuraciones de la matriz de vista/proyección
-    glm::mat4 projection = glm::perspective(glm::radians(50.0f), (float)gl_width / (float)gl_height, 0.1f, 1000.0f);
-    glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(projection));
+  // Activa la textura
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glUniform1i(glGetUniformLocation(shader_program, "tex"), 0);
 
-    // Matriz de modelo con rotaciones y traslaciones
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -4.0f));
-    model = glm::translate(model, glm::vec3(sinf(2.1f * f) * 0.5f, cosf(1.7f * f) * 0.5f, sinf(1.3f * f) * cosf(1.5f * f) * 2.0f));
-    model = glm::rotate(model, glm::radians((float)currentTime * 45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    model = glm::rotate(model, glm::radians((float)currentTime * 81.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  // Configuraciones de la matriz de vista/proyección
+  glm::mat4 projection = glm::perspective(glm::radians(50.0f), (float)gl_width / (float)gl_height, 0.1f, 1000.0f); // Cambiar a radians a 10.0f para ver mas de cerca
+  glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(projection));
 
-    // Envía las matrices al shader
-    glUniformMatrix4fv(mv_location, 1, GL_FALSE, glm::value_ptr(model));
+  // Matriz de modelo con rotaciones y traslaciones
+  glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -4.0f));
+  model = glm::translate(model, glm::vec3(sinf(2.1f * f) * 0.5f, cosf(1.7f * f) * 0.5f, sinf(1.3f * f) * cosf(1.5f * f) * 2.0f));
+  model = glm::rotate(model, glm::radians((float)currentTime * 45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+  model = glm::rotate(model, glm::radians((float)currentTime * 81.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  //model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // Comentar las 3 lineas anteriores y descomentar esta para ver la textura
 
-    glBindVertexArray(vao);
+  // Envía las matrices al shader
+  glUniformMatrix4fv(mv_location, 1, GL_FALSE, glm::value_ptr(model));
 
-    // Define si aplicar la textura o no
-    GLint applyTextureLoc = glGetUniformLocation(shader_program, "applyTexture");
+  glBindVertexArray(vao);
 
-    // Dibuja la cara texturizada
-    glUniform1i(applyTextureLoc, GL_TRUE);
-    glDrawArrays(GL_TRIANGLES, 0, 6); // Dibuja solo la cara frontal
+  // Define si aplicar la textura o no
+  GLint applyTextureLoc = glGetUniformLocation(shader_program, "applyTexture");
 
-    // Dibuja el resto del cubo sin textura
-    glUniform1i(applyTextureLoc, GL_FALSE);
-    glDrawArrays(GL_TRIANGLES, 6, 36 - 6); // Dibuja el resto del cubo
+  // Dibuja la cara texturizada
+  glUniform1i(applyTextureLoc, GL_TRUE);
+  glDrawArrays(GL_TRIANGLES, 0, 6); // Dibuja solo la cara frontal
 
+  // Dibuja el resto del cubo sin textura
+  glUniform1i(applyTextureLoc, GL_FALSE);
+  glDrawArrays(GL_TRIANGLES, 6, 36 - 6); // Dibuja el resto del cubo
 }
 
-
-void processInput(GLFWwindow *window) {
-  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, 1);
 }
 
 // Callback function to track window size and update viewport
-void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
+void glfw_window_size_callback(GLFWwindow *window, int width, int height)
+{
   gl_width = width;
   gl_height = height;
   printf("New viewport: (width: %d, height: %d)\n", width, height);
+  glViewport(0, 0, width, height); // Si aumentamos o reducimos la pantalla se ajusta
 }
