c9a6754854c65d3b92b4c06bc3f4ccd7f29c1644
adrian-gude <adrianlopezgude@gmail.com>
2024-05-03 12:01:23 +0200

    pair programing with Juan

--- practica_cubo_osg.cpp
+++ practica_cubo_osg.cpp
@@ -8,6 +8,7 @@
 #include <osgViewer/Viewer>
 
 #include <osg/Timer>
+#include <osg/MatrixTransform>
 
 // Crear un timer global
 static osg::Timer globalTimer;
@@ -22,10 +23,42 @@ CreateSubGraph(osg::ref_ptr<osg::Group> root,
 
    root->addChild(pat);
    pat->addChild(model);
+   //pat->setDataVariance(osg::Object::DYNAMIC);
    pat->setPosition(translation);
 
    return pat;
 }
+
+
+class RotateCB : public osg::NodeCallback
+{
+public:
+    RotateCB() : _angle(0.) {}
+    virtual void operator()(osg::Node* node, osg::NodeVisitor* nv)
+    {
+        osg::PositionAttitudeTransform* pat =
+            dynamic_cast<osg::PositionAttitudeTransform*>(node);
+        if (pat)
+        {
+            // Calcular nueva orientación
+            osg::Quat newAttitude;
+            newAttitude.makeRotate(_angle, osg::Vec3(0., 0., 1.)); // Rotación alrededor del eje Z
+            
+            // Configurar la posición y la orientación
+            pat->setPosition(osg::Vec3(-6., 0., 0.)); // Traslación constante
+            pat->setAttitude(newAttitude);
+
+            // Incrementar el ángulo para la próxima iteración
+            _angle += 0.01;
+
+            // Continuar con el recorrido
+            traverse(node, nv);
+        }
+    }
+
+protected:
+    double _angle;
+};
 
 int main(int argc, char *argv[])
 {
@@ -45,13 +78,6 @@ int main(int argc, char *argv[])
       exit(1);
    }
 
-   /*
-      glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -4.0f));
-      model = glm::translate(model, glm::vec3(sinf(2.1f * f) * 0.5f, cosf(1.7f * f) * 0.5f, sinf(1.3f * f) * cosf(1.5f * f) * 2.0f));
-      model = glm::rotate(model, glm::radians((float)currentTime * 45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-      model = glm::rotate(model, glm::radians((float)currentTime * 81.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-   */
-
    // Create the scene graph
    // const double translation = 2.2 * loadedModel->getBound().radius(); // (1)
    double currentTime = globalTimer.time_s();
@@ -65,6 +91,8 @@ int main(int argc, char *argv[])
 
    osg::ref_ptr<osg::PositionAttitudeTransform> spinningCube =
        CreateSubGraph(root, loadedModel, translation);
+   spinningCube->setDataVariance(osg::Object::DYNAMIC);
+   spinningCube->setUpdateCallback(new RotateCB);
 
 
    // Play with the StateSets
